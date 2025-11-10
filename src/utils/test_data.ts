f356319a2034a070726283c7c665e471dfd6a7b6
Javi <jgjnez@gmail.com>
2025-11-10 11:52:10 -0600

    Creating test data

---
+++ src/utils/test_data.ts
@@ -0,0 +1,36 @@
+export const project = [
+  {
+    id: 1,
+    name: 'Project Alpha',
+    description: 'This is the first project.',
+  },
+  {
+    id: 2,
+    name: 'Project Beta',
+    description: 'This is the second project.',
+  },
+]
+
+export const tasks = [
+  {
+    id: 1,
+    projectId: 1,
+    title: 'Design the UI',
+    description: 'Loren ip su sd fe kfds',
+    completed: false,
+  },
+  {
+    id: 2,
+    projectId: 1,
+    title: 'Set up the database',
+    description: 'Loren ip su sd fe kfds',
+    completed: true,
+  },
+  {
+    id: 1,
+    projectId: 2,
+    title: 'Design the UI',
+    description: 'Loren ip su sd fe kfds',
+    completed: false,
+  },
+]
